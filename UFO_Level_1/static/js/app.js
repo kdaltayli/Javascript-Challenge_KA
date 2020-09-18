64beace5f51dba8075632932850e78063b550cbb
Kursad Altayli <kdaltayli@gmail.com>
2020-09-17 22:53:30 -0400

    updated

---
+++ UFO_Level_1/static/js/app.js
@@ -0,0 +1,54 @@
+// from data.js
+var tableData = data;
+console.log(tableData);
+
+// function delete_duration(time){
+//     if (time === "durationMinutes"){
+//         delete time.durationMinutes;
+//     };
+// };
+
+// var newData=tableData.filter(delete_duration);
+// console.log(newData);
+var tbody = d3.select("tbody");
+
+tableData.forEach(function(ufo){
+    console.log(ufo);
+    var row =tbody.append("tr");
+
+    Object.entries(ufo).forEach(function([key,value]){
+        console.log(key,value);
+        var cell =row.append("td");
+        cell.text(value);
+    });
+});
+// //Refactor to use Arrow Functions!
+// tableData.forEach((ufo) => {
+//     var row = tbody.append("tr");
+//     Object.entries(ufo).forEach(([key, value]) => {
+//       var cell = row.append("td");
+//       cell.text(value);
+//     });
+//   });
+
+//Use a date form in your HTML document and write JavaScript code that will listen for events 
+//and search through the `date/time` column to find rows that match user input.
+
+//Select the button
+var button = d3.select("#filter-btn");
+
+//Select the form
+var form = d3.select("form");
+
+//Create event handlers for clicking the button or pressing the enter key
+button.on("click",runEnter);
+form.on("click", runEnter);
+
+function runEnter() {
+    //select the input element and get the raw html node
+    var inputElement = d3.select(".form-control");
+
+    //Get the value property of the input element
+    var inputValue= inputElement.property("value");
+
+};
